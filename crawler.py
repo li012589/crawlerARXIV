63c60d38094e394c5008ae6d21be49ed9e1dff31
Li SH <li012589li@outlook.com>
2017-04-09 18:27:16 +0800

    optimize setting

--- crawler.py
+++ crawler.py
@@ -1,27 +1,9 @@
 import threading
 import re
+from setting import Settings
 
 # Const Vars
 settingPath = './setting.txt'
-
-class Settings:
-    _settings={'settingPath':'./setting.txt', 'url':'https://arxiv.org', 'keyword':'','outputPath':'./OUTPUT', 'resultPath':'./OUTPUT/result.md', 'isdownload':0}
-    def addKey(self,key,value):
-        self._settings[key]=value
-    def getValue(self,key):
-        return self._settings[key]
-    def updKey(self,key,value):
-        pass
-    def __init__(self,path):
-        pattern=r"^//"
-        with open(path,'r') as f:
-            settings = f.read()
-            settings = settings.split('\n')
-            for line in settings:
-                if re.search(pattern,line):
-                    continue
-                line=line.split(' ')
-                self._settings[line[0]]=line[2]
 
 if __name__ == "__main__":
     settings=Settings(settingPath)
