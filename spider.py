9a864d5337e5759eb95e246df6029144da7b8681
Li SH <li012589li@outlook.com>
2017-04-10 01:31:25 +0800

    add spider.py file

---
+++ spider.py
@@ -0,0 +1,65 @@
+from html.parser import HTMLParser
+from urllib.request import urlopen
+from urllib import parse
+import fileOperator as fo
+
+class linksFinder(HTMLParser):
+    def __init__(self,baseUrl):
+        super().__init__()
+        self.base_url = baseUrl
+        self.links = set()
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for (attribute, value) in attrs:
+                if attribute == 'href':
+                    url = parse.urljoin(self.base_url, value)
+                    self.links.add(url)
+
+class Spider:
+    todoPath = ''
+    donePath = ''
+    baseUrl = ''
+    todo = set()
+    done = set()
+    domain = ''
+    def __init__(self,baseUrl,temPath):
+        Spider.baseUrl = baseUrl
+        Spider.todoPath = temPath + '/todolist.txt'
+        Spider.donePath = temPath + '/donelist.txt'
+        Spider.domain = baseUrl
+        fo.createDir(temPath)
+        fo.createFile(Spider.todoPath)
+        fo.createFile(Spider.donePath)
+        Spider.todo = set(fo.file2list(Spider.todoPath))
+        Spider.todo.add(Spider.baseUrl)
+        Spider.done = set(fo.file2list(Spider.donePath))
+        #print('Starting crawling')
+        self.crawl('1st',Spider.baseUrl)
+
+    @staticmethod
+    def gatherURL(url):
+        html=''
+        try:
+            response = urlopen(url)
+            if 'text/html' in response.getheader('Content-Type'):
+                html=response.read()
+                html=html.decode('utf-8')
+            res = linksFinder(Spider.baseUrl)
+            res.feed(html)
+            return res.links, html
+        except Exception as e:
+            print(str(e))
+            return set(),html
+
+
+    @staticmethod
+    def crawl(name,url):
+        if url not in Spider.done:
+            newUrl,contents=Spider.gatherURL(url)
+            for iterm in newUrl:
+                if (iterm not in Spider.done) and (iterm not in Spider.todo) and (Spider.domain in iterm):
+                    Spider.todo.add(iterm)
+            Spider.todo.remove(url)
+            Spider.done.add(url)
+            fo.list2file(Spider.todoPath,list(Spider.todo))
+            fo.list2file(Spider.donePath,list(Spider.done))
