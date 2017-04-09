2bb88f6593a2a33e873b2d5d33cd1ef51c69ee72
Li SH <li012589li@outlook.com>
2017-04-09 17:08:49 +0800

    basic finished

---
+++ crawler.py
@@ -0,0 +1,17 @@
+import threading
+
+settingPath = './setting.txt'
+url = 'https://arxiv.org'
+keyword = []
+outputPath = './OUTPUT'
+resultPath = './OUTPUT/result.md'
+
+def readSetting(path='./setting.txt'):
+    pass
+    return url,keyword,outputPath,resultPath
+
+
+if __name__ == "__main__":
+    readSetting(settingPath)
+
+
